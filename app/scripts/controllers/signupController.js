ed3f2295fde30fa6395b7a96beb98121c53e84eb
Tiago Quaini <tiago.quaini@sap.com>
2017-03-31 15:38:23 -0300

    finalize signup page

--- app/scripts/controllers/signupController.js
+++ app/scripts/controllers/signupController.js
@@ -2,8 +2,30 @@
 
   "use strict";
 
-  angular.module('socsystem').controller('SignupController', ['$scope', '$translate',
-    function( $scope, $translate ) {
+  angular.module('socsystem').controller('SignupController', ['$scope', '$translate', 'UserService',
+    function( $scope, $translate, UserService ) {
+
+      function _validateForm() {
+        if (!$scope.name || $scope.name.trim() === "" 
+            || !$scope.email || $scope.email.trim() === "" 
+            || !$scope.password || $scope.password.trim() === "") {
+          return false;
+        }
+        return true;
+      }
+
+      $scope.createUser = function() {
+        if (!_validateForm()) {
+          _showErrorModal();
+          return;
+        }
+
+        UserService.createUser({
+          name: $scope.name,
+          email: $scope.email,
+          password: $scope.password
+        });
+      };
 
     }
   ]);
