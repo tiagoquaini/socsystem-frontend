ff3dc5d285bf0744ede318b390dbf12f0b4c2258
Tiago Quaini <tiago.quaini@sap.com>
2017-03-31 16:08:43 -0300

    created signup screen

--- app/scripts/controllers/signupController.js
+++ app/scripts/controllers/signupController.js
@@ -2,29 +2,34 @@
 
   "use strict";
 
-  angular.module('socsystem').controller('SignupController', ['$scope', '$translate', 'UserService',
-    function( $scope, $translate, UserService ) {
+  angular.module('socsystem').controller('SignupController', ['$scope', 'UserService',
+    function( $scope, UserService ) {
 
-      function _validateForm() {
+      $scope.disableRegisterBtn = function() {
         if (!$scope.name || $scope.name.trim() === "" 
             || !$scope.email || $scope.email.trim() === "" 
             || !$scope.password || $scope.password.trim() === "") {
-          return false;
+          return true;
         }
-        return true;
+        return false;
+      }
+
+      function _userCreateError() {
+        $(".error-modal").modal();
+      }
+
+      function _userCreateSuccess(oUser) {
+
       }
 
       $scope.createUser = function() {
-        if (!_validateForm()) {
-          _showErrorModal();
-          return;
-        }
-
         UserService.createUser({
           name: $scope.name,
           email: $scope.email,
           password: $scope.password
-        });
+        })
+        .then(_userCreateSuccess)
+        .catch(_userCreateError);
       };
 
     }
