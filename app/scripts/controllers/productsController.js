50ac345a9787fb8f09992a5bba7f112ac0913f69
Tiago Quaini <tiago.quaini@sap.com>
2017-05-31 15:56:58 -0300

    created products list screen

--- app/scripts/controllers/productsController.js
+++ app/scripts/controllers/productsController.js
@@ -2,10 +2,17 @@
 
   "use strict";
 
-  angular.module('socsystem').controller('ProductsController', ['$scope', '$state', 'UserService',
-    function( $scope, $state, UserService ) {
+  angular.module('socsystem').controller('ProductsController', ['$scope', '$state', 'UserService', 'ProductService',
+    function( $scope, $state, UserService, ProductService ) {
 
       $scope.user = UserService.getLoggedUser();
+
+      $scope.aProducts = [];
+
+      ProductService.getAllProducts()
+      .then(function(aProducts) {
+        $scope.aProducts = aProducts;
+      });
 
     }
   ]);
